8c9756bdfe887cd92bfcf4879fb8f81357adfa5d
Mohamed-Lamine Ngom <lamzo@MacBook-Pro-de-Mohamed-Lamine.local>
2023-11-07 16:26:47 +0100

    MLN : feature starting navbar

---
+++ e-commerce_frontoffice/src/Components/Topbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Topbar = () => {
+  return (
+    <div className='topbar'>
+        <div className='left-side'>
+            <img src="../Ressources/Images/ecostore_logo.png" alt="logo" className="logo" />
+        </div>
+        <div className='right-side'>
+            cart and log
+        </div>
+    </div>
+  )
+}
+
+export default Topbar
