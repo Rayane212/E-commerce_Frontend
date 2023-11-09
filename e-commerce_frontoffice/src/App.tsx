a945577ff70105bd039d95371fd88cfc1d7bb4cd
Mohamed-Lamine Ngom <lamzo@MacBook-Pro-de-Mohamed-Lamine.local>
2023-11-09 14:23:58 +0100

    MLN/feature : setup folders architecture

--- e-commerce_frontoffice/src/App.tsx
+++ e-commerce_frontoffice/src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import './App.css';
-import Topbar from './Components/Topbar'
 
 function App() {
   return (
     <div className="App">
-      <Topbar/>
     </div>
   );
 }
