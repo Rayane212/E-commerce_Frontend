7ec4e8a324a49b339d9137f317f0f1059780e2af
Mohamed-Lamine Ngom <lamzo@MacBook-Pro-de-Mohamed-Lamine.local>
2023-11-09 16:28:32 +0100

    MLN add navbar list items

---
+++ e-commerce_frontoffice/src/components/Navbar.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <ul>
+            <li>Home</li>
+            {/* desktop only */}
+            <li>About</li>
+            {/* desktop only */}
+            <li>Contact us</li>
+            {/* desktop only */}
+            <li>Blog</li>
+            {/* desktop only */}
+
+            <li>heart</li>
+            <li>cart</li>
+            <li>log</li>
+            
+            <li>burger</li>
+            {/* mobile only */}
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
