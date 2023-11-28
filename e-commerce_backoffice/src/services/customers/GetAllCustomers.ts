302c3ff69366ba4ffa0ff0156bbd75ab384f33b4
Hamza Sabil <hmzsabil@gmail.com>
2023-11-28 11:27:54 +0100

    creation hook persos

--- e-commerce_backoffice/src/services/customers/GetAllCustomers.ts
+++ e-commerce_backoffice/src/services/customers/GetAllCustomers.ts
@@ -2,9 +2,14 @@ import React from 'react'
 import customers from '../../assets/json/customers.json';
 import { Customer } from '../../models/Customer';
 
+let isRequest : boolean = false;
 
 function GetAllCustomers(){
+    if(isRequest){return []}
+    isRequest = true;
     const data : Customer [] = customers; 
+    console.log('GetAllCustomers');
+    isRequest = false;
     return data
 }
 export default GetAllCustomers;
