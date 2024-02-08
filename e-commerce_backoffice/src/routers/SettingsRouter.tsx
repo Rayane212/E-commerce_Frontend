322096d2f1709f606056f17d7c9bd85c76d6cbf6
Hamza Sabil <hmzsabil@gmail.com>
2024-02-08 11:17:39 +0100

    fix settings router bugs

--- e-commerce_backoffice/src/routers/SettingsRouter.tsx
+++ e-commerce_backoffice/src/routers/SettingsRouter.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter as Router, Route, Routes, Link, useNavigate } from 'react-router-dom';
 import GeneralSettings from '../views/settings/general/GeneralSettings';
 import UsersSettings from '../views/settings/users/UsersSettings';
@@ -9,42 +9,51 @@ import PoliticSettings from '../views/settings/politics/PoliticSettings';
 const SettingsRouter: React.FC = () => {
     const windowLocation = window.location.pathname;
     const navigate = useNavigate();
-    if (windowLocation.includes('/settings')) {
-        const slicedPath = windowLocation.slice(10);
-        if(slicedPath === '') {
+    
+
+    useEffect(() => {
+      const slicedPath = windowLocation.slice(10);
+        if (slicedPath === '') {
             navigate('/settings/general');
         }
-        else if(slicedPath === 'general') {
-            return (
-                <GeneralSettings/>
-            )
-        }
-        else if(slicedPath === 'users') {
-            return (
-                <UsersSettings/>
-            )
-        }
-        else if (slicedPath === "payments"){
+    }, [windowLocation])
+
+    if (windowLocation.includes('/settings')) {
+      const slicedPath = windowLocation.slice(10);
+      if(slicedPath === 'general') {
           return (
-            <PaymentSettings/>
+              <GeneralSettings/>
           )
-        }
-        else if (slicedPath === "shipping_method"){
+      }
+      else if(slicedPath === 'users') {
           return (
-            <ShippingMethodSettings/>
+              <UsersSettings/>
           )
-        }
-        else if (slicedPath === "politics"){
-          return (
-            <PoliticSettings/>
-          )
-        }
-        else{
-          return(<></>)
-        }
+      }
+      else if (slicedPath === "payments"){
+        return (
+          <PaymentSettings/>
+        )
+      }
+      else if (slicedPath === "shipping_method"){
+        return (
+          <ShippingMethodSettings/>
+        )
+      }
+      else if (slicedPath === "politics"){
+        return (
+          <PoliticSettings/>
+        )
+      }
+      else{
+        return(<></>)
+      }
     }
-    return(<></>)
-  
+    else{
+      return(<></>)
+    }
+
+      
 }
 
 export default SettingsRouter
